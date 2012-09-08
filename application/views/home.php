71ed1e3c41f78782cc0df30b1ae989ff96bbaeb4
Hafiz Usman Ahmad (Gosign) <hafizusman@gosign.de>
2012-09-08 06:39:46 +0500

    if text field is empty then a nice error will show and hide after 2 sec

--- application/views/home.php
+++ application/views/home.php
@@ -2,7 +2,10 @@
 	<div class="span12">
 		
 		<div id="searchBox">
-
+		<div class="alert alert-error" style="display:none; position:absolute;margin-top:-62px;width:396px;">
+  <button type="button" class="close" data-dismiss="alert">x</button>
+  Text field is empty.
+</div>
 	    <h1>Explore the world of ideas</h1>
 			<form class="navbar-search" id="srchForm" method="GET">
 			  <input type="text" name="q" class="search-query" placeholder="Search Ideas" style="width:420px">
@@ -21,6 +24,10 @@
 		});
 		$('#srchForm').submit(function(){
 			if($('#searchBox').css('margin-top') == '150px') {
+			if($('input[name=q]').text().trim() == '') {
+				$('#searchBox .alert').stop(true,true).fadeIn().delay(2000).fadeOut();
+				return false;
+			}
 			$('#searchBox').animate({
 				marginTop: '-=150'
 			});
