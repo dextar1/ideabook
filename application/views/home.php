da05b7dee62577ad70b3c2eff800b6b1caac22ea
Usman Ahmad <hafizusman@gosign.de>
2012-09-15 22:21:40 +0500

    synced

--- application/views/home.php
+++ application/views/home.php
@@ -2,7 +2,6 @@
 	<div class="span12">
 		
 		<div id="searchBox">
-<<<<<<< HEAD
 		<div class="alert alert-error" style="display:none; position:absolute;margin-top:-62px;width:396px;">
 		  <button type="button" class="close" data-dismiss="alert">x</button>
 		  Text field is empty.
@@ -33,36 +32,4 @@
 		});
 	});
 </script>
-=======
-		<div class="alert alert-error" style="display:none; position:absolute;margin-top:-62px;width:396px;">
-  <button type="button" class="close" data-dismiss="alert">x</button>
-  Text field is empty.
-</div>
-	    <h1>Explore the world of ideas</h1>
-			<form class="navbar-search" id="srchForm" method="GET">
-			  <input type="text" id="searchQuery" name="q" class="search-query" placeholder="Search Ideas" style="width:420px">
-			<div class="hint">
-				<a class="btn btn-small srchBtn" href="#"><i class="icon-search"></i></a>
-			</div>
-			</form>
-			<div class="results"></div>
-		</div>
-	</div>
-</div>
-<script>
-	$(function(){
-		$('.srchBtn').click(function(){
-			$('#srchForm').submit();
-			return false;
-		});
-		$('#srchForm').submit(function(){
-			
-			if($('#searchQuery').val().trim() == '') {
-				$('#searchBox .alert').stop(true,true).fadeIn().delay(2000).fadeOut();
-				return false;
-			}
-			return false;
-		});
-	});
-</script>
->>>>>>> input changes
+
